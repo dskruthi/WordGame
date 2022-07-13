fbbb29212344c4b64eb4ef2f25f3717a35f74ac1
Marielle Belomme <89320026+mbelomme@users.noreply.github.com>
2022-07-13 02:13:36 -0400

    Adds function startGame() to load game session.

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let triesLeft = 0;
 let numCorrect = 0;
 let userCorrectAnswers = [];
 let correctAnswers;
+
+// Fetches q&a file and selects a random question for the current game session.
+async function startGame() {
+    try {
+        let promise = await fetch('./q&a.json');
+        let json = await promise.json();
+        let question = json[Math.floor(json.length * Math.random())];
+        document.getElementById("question").innerHTML = question["question"];
+        // correctAnswers = question["answers"]
+        return question["answers"]
+    } catch(error) {
+        console.log(error);
+    }
+}
